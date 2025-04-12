b37ef086ad835014e33f632392fa944229aa5790
oviekshagya <oviekshgy@gmail.com>
2025-04-12 15:47:26 +0700

    Add cors

--- api/middleware/cors.go
+++ api/middleware/cors.go
@@ -7,7 +7,7 @@ import (
 
 func CORSMiddleware() fiber.Handler {
 	return cors.New(cors.Config{
-		AllowOrigins:     "https://shagya-tech.my.id", //Deployment
+		AllowOrigins:     "https://shagya-tech.my.id, http://localhost:8080", //Deployment
 		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
 		AllowHeaders:     "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, X-API-KEY, X-SIGNATURE, X-TIMESTAMP",
 		AllowCredentials: true,
